bbc41c03c6999a14fc00e164abb6a60e97bb1ba2
NetworkCrab <network123crab@gmail.com>
2023-07-26 14:02:14 +0530

    practise and understanding more on introdunction to c++ basic

---
+++ c++/practise_introdunction.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	// Declaring a variable
+	int sum = 6;
+	// Printing the basic hello world
+	cout << "Hello World\n" << sum;
+	return 0;
+	// This is a variable
+	
+}
+
+// Writing the command
+
